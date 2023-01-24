67ce58f1ac7fe17567e41c1a182afd9f78db207a
przemo179 <przemus17889@gmail.com>
2023-01-24 12:14:03 +0100

    Remove the now ignored directory node_modules

--- src/dashboard/components/Dashboard.jsx
+++ src/dashboard/components/Dashboard.jsx
@@ -1,5 +1,11 @@
 import IngredientsSearchBar from "./IngredientsSearchBar";
+import ProductsList from "./IngredientsList"
 
 export const Dashboard = () => {
-    return <IngredientsSearchBar />
+    return (
+    <div>
+        <IngredientsSearchBar />
+        <ProductsList />
+    </div>  
+    )
 }
